d3ea3936b3fb902739973021d115b566cc8c0e84
oluoko <brianotieno586@gmail.com>
2024-08-04 05:44:41 -0400

    Brian's Portfolio

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  darkMode: "class", // This enables dark mode using the 'class' strategy
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
