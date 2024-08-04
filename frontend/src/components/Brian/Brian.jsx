94c9cc9d03e138d0910665e4af36a33d1450292d
oluoko <brianotieno586@gmail.com>
2024-08-04 04:35:20 -0400

    Brian's Portfolio

--- frontend/src/components/Brian/Brian.jsx
+++ frontend/src/components/Brian/Brian.jsx
@@ -1,6 +1,5 @@
 import styles from "./Brian.module.css";
 import { getImageUrl } from "../../utils";
-import { FaArrowDown } from "react-icons/fa";
 
 export const Brian = () => {
   return (
@@ -11,41 +10,12 @@ export const Brian = () => {
           A software developer with a passion for building web applications.
           Currently working as a full-stack developer.
         </p>
-        <a
-          className={styles.contactBtn}
-          href="mailto:brianotieno586@gmail.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Contact Me
-        </a>
-        <div className={styles.resumeContainer}>
-          <a
-            href="https://docs.google.com/document/d/1r75YJhfcUEZvzuFYd0IwwWSfLJ9gWgq0yaF3PpYrA_Q/edit?usp=sharing"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.resume}
-          >
-            Resume
-          </a>
-
-          <a
-            href="https://docs.google.com/document/d/1r75YJhfcUEZvzuFYd0IwwWSfLJ9gWgq0yaF3PpYrA_Q/edit?usp=sharing"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.downloadButton}
-          >
-            <FaArrowDown />
-          </a>
-        </div>
       </div>
       <img
         src={getImageUrl("Brian/brian-s gif.gif")}
         alt="An image of Brian, the creator."
         className={styles.brianImg}
       />
-      {/* <div className={styles.topBlur} />
-      <div className={styles.bottomBlur} /> */}
     </section>
   );
 };
