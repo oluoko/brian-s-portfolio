a07fa413c99c3e67fe42ed0cbe50a27bffcc2923
oluoko <brianotieno586@gmail.com>
2024-08-05 07:42:45 -0400

    Brian's Portfolio: frontend/src/components/Gallery.jsx

--- frontend/src/components/Brian/Brian.jsx
+++ frontend/src/components/Brian/Brian.jsx
@@ -7,12 +7,11 @@ export const Brian = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hello, I&apos;m Brian Otieno.</h1>
         <p className={styles.description}>
-          I am a dedicated software developer with a passion for creating
-          dynamic and efficient web applications. With a solid background in
-          full-stack development, mainly using the MERN stack, I specialize in
-          both front-end and back-end technologies to ensure seamless and robust
-          solutions. Currently, I am honing my skills as a full-stack developer,
-          continuously striving to innovate and enhance the user experience.
+          I am a passionate software developer specializing in dynamic web
+          applications using the MERN stack. With expertise in both front-end
+          and back-end technologies, I ensure seamless and robust solutions.
+          Currently, I am honing my full-stack skills, always striving to
+          innovate and enhance user experience.
         </p>
       </div>
       <img
